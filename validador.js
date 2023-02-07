dd16583b25e416a248cbeaed54a0767f52b9c40f
Gustavo Cintrão dos Santos <182200280@senacinformatica.br>
2023-02-07 17:32:49 -0300

    g

--- validador.js
+++ validador.js
@@ -83,11 +83,71 @@ function mascaraCNPJ(i) {
 
 function validar(){
     var nome = formUser.nome.value;
-    
-    
+    var email = formUser.email.value;
+    var tel = formUser.fone.value;
+    var endereço = formUser.end.value;
+    var cpf = formUser.cpf.value;
+    var cnpj = formUser.cnpj.value;
+    var senha = formUser.senha.value;
+    var confirmsenha = formUser.confirmsenha.value;
+   
+
     if (nome == "" || nome.length <=3) {
         alert("informe o nome completo!");
         formUser.nome.focus();
         return false;
     }
+    if (email == "" || email.length <=10) {
+        alert("informe o email completo!");
+        formUser.email.focus();
+        return false;
+    }
+    if (tel == "" || tel.length <=15) {
+        alert("informe o telefone completo!");
+        formUser.fone.focus();
+        return false;
+    }
+    if (endereço == "" || endereço.length <=12) {
+        alert("informe o endereço completo!");
+        formUser.end.focus();
+        return false;
+    }
+
+    if (cpf == "" && cnpj == "") {
+        alert("Selecione um documento");
+        return false;
+    }
+   
+    if (cpf != "" && cnpj =="") {
+        if(cpf.length !=14){
+        alert("CPF incorreto");
+        formUser.cpf.focus();
+        return false;
+    }
+}
+
+if (cnpj != "" && cpf =="") {
+    if(cnpj.length !=18){
+    alert("CNPJ incorreto");
+    formUser.cnpj.focus();
+    return false;
+    }
+  }
+
+  if (senha == "" || senha.length < 8) {
+    alert("Senha não atende aos requisitos!");
+    formUser.senha.focus();
+    return false;
+}
+
+  if (senha != "" || confirmsenha != "") {
+      if (senha != confirmsenha) {
+        alert("Senhas não conferem!");
+        formUser.senha.focus();
+        return false;
+      }
+
+  }
+
+
 } 
